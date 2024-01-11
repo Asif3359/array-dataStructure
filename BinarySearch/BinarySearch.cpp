fa82e5f559140ab7addb8542fba52c80a5095ee2
Asif3359 <asifahammed359@gmail.com>
2024-01-11 23:44:31 +0600

    add binarySearch algo

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 using namespace std;
 
+void binarySearch(int array[], int n, int k)
+{
+    int start = 0;
+    int end = n-1;
+    int mid;
+    while (start < end)
+    {
+        mid = (start + end) / 2;
+        if (array[mid] == k)
+        {
+            cout << "Found " << endl;
+            return;
+        }
+        else if (array[mid] < k)
+        {
+            start = mid + 1;
+            mid = (start + end) / 2;
+        }
+        else if (array[mid] > k)
+        {
+            end = mid - 1;
+            mid = (start + end) / 2;
+        }
+    }
+    cout << "not Found"<<endl;
+}
+
 int main()
 {
     // implement Binary search
-    int array[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array[100];
+    int n;
+    cout << "Enter size of array " << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    int k;
+    cout << "Enter key " << endl;
+    cin >> k;
+    binarySearch(array, n, k);
 
-   return 0 ;
+    return 0;
 }
