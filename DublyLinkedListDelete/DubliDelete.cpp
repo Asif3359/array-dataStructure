ed5526d7873da0ac91c6e96aa0d1174de2168dd1
Asif3359 <asifahammed359@gmail.com>
2024-01-15 02:03:17 +0600

    delete at given position

--- DublyLinkedListDelete/DubliDelete.cpp
+++ DublyLinkedListDelete/DubliDelete.cpp
@@ -47,6 +47,7 @@ public:
             temp = newNode;
             temp->next = nullptr;
         }
+        count++;
     }
 
     void deleteFirst()
@@ -56,11 +57,23 @@ public:
         head = head->next;
         head->prev = nullptr;
         free(temp);
+        count--;
     }
 
     void deleteLast()
     {
         temp = head;
+        if (head == nullptr)
+        {
+            cout << "No item" << endl;
+            return;
+        }
+        if (head->next == nullptr)
+        {
+            delete head;
+            head = nullptr;
+            return;
+        }
         while (temp->next != nullptr)
         {
             temp = temp->next;
@@ -68,6 +81,55 @@ public:
         temp = temp->prev;
         temp->next = nullptr;
         free(temp->next);
+
+        count--;
+    }
+    void deleteAtPosition(int position)
+    {
+        if (head == nullptr)
+        {
+            // Empty list, nothing to delete
+            return;
+        }
+
+        if (position == 1)
+        {
+            // Special case: deleting the head node
+            temp = head;
+            head = head->next;
+            if (head != nullptr)
+            {
+                head->prev = nullptr;
+            }
+            delete temp;
+            return;
+        }
+
+        Node *current = head;
+        int currentPosition = 1;
+
+        while (currentPosition < position && current != nullptr)
+        {
+            current = current->next;
+            currentPosition++;
+        }
+
+        if (current == nullptr)
+        {
+            // Position is greater than the number of nodes in the list
+            return;
+        }
+
+        // Adjust pointers to skip the node at the specified position
+        current->prev->next = current->next;
+
+        if (current->next != nullptr)
+        {
+            current->next->prev = current->prev;
+        }
+
+        // Delete the node at the specified position
+        delete current;
     }
 };
 
@@ -77,12 +139,15 @@ int main()
 
     list.creatNode(1);
     list.creatNode(2);
+    list.creatNode(10);
     list.creatNode(3);
     list.creatNode(4);
     list.creatNode(5);
 
     list.deleteFirst();
-    list.deleteLast();
+    // list.deleteLast();
+
+    list.deleteAtPosition(3);
 
     Node *current = list.getHead();
 
