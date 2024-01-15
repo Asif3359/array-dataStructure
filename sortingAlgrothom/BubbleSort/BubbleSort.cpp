48faa1f13c8bcbbb3b4c4d9911693d2ba9e0dd99
Asif3359 <asifahammed359@gmail.com>
2024-01-15 22:09:16 +0600

    add bubble sort time(n)

--- sortingAlgrothom/BubbleSort/BubbleSort.cpp
+++ sortingAlgrothom/BubbleSort/BubbleSort.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
-int main ()
-{
 
-    return 0 ;
+void swapp(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int arr[] = {2, 1, 7, 3, 10, 4};
+
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-1 ; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapp(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
 }
