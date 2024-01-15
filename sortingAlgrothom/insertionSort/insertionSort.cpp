103a00515dd700f364e61ce71ea42bba7fc48172
Asif3359 <asifahammed359@gmail.com>
2024-01-15 22:37:38 +0600

    add insertion sort time(n)

---
+++ sortingAlgrothom/insertionSort/insertionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 3, 9, 4, 8, 2};
+    int n = 6;
+    int key;
+
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
